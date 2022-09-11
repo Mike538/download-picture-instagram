0fedca268dc4625c514f1b63ea131312b77e037f
Mike <75444827+Mike538@users.noreply.github.com>
2022-09-11 00:16:29 -0500

    Update download instagram.py

--- download instagram.py
+++ download instagram.py
@@ -1,8 +1,10 @@
-# Importamos la librería instaloader necesaria para el funcionamiento del programa
+# Importamos la librería instaloader necesaria para el funcionamiento del pro
 # Si no posees la librería introduce este comando sin las comillas en tu terminal ↓↓↓
 # comando "pip install instaloader"
 
 
+import signal
+import string
 import instaloader
 from time import sleep
 from tqdm import tqdm
@@ -12,10 +14,17 @@ import requests
 parser = instaloader.Instaloader()
 username = input("Introduzca el nombre del usuario: ")
 print()
-print("se comenzara a descargar el perfil de",username)
-print()
-parser.download_profile(username,profile_pic=True)
+texto = input("Esta seguro de descargar este perfil? (si/no): ")
+if texto == "si":
+    print("se comenzara a descargar el perfil de",username)
+    print()
+    parser.download_profile(username,profile_pic=True)
+    print()
 #Los archivos se descargaran en la ruta donde tenga el programa 
-print("Se descargó por completo el perfil de ",username)
-print()
-print("Gracias por utilizar el programa :)")
+    print("Se descargó por completo el perfil de: ",username)
+    print()
+    print("Gracias por utilizar el programa :)")
+
+else:
+    print("Hasta la proxima, adios")
+#print("se comenzara a descargar el perfil de",username)
